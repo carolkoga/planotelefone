aa82b60bcad9ac04102c53ac863d27b9b62ad260
Carol Koga <caroltkoga@gmail.com>
2025-04-19 19:29:57 -0300

    inicio plano telefone basico

---
+++ planotelefone.java
@@ -0,0 +1,15 @@
+Public class planotelefone{
+    public static void main(String [] args){
+        //when min > 100, preço total: min - 100 * 2.
+        int valorPlano;
+        int minExtra;
+        int minTotal;
+        int valorExtra;
+        valorPlano = 50;
+        if (minTotal > 100){
+            minExtra = minTotal - 100;
+            valorExtra = minExtra*2;
+            
+        }
+    }
+}
