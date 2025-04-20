51ceb9e720d34f9ed2a6fcc7040f41286bd4e2a4
Carol Koga <caroltkoga@gmail.com>
2025-04-19 23:07:13 -0300

    Sistema Teste ok

--- planotelefone.java
+++ planotelefone.java
@@ -1,15 +1,31 @@
-Public class planotelefone{
+import java.util.Scanner;
+public class planotelefone{
     public static void main(String [] args){
         //when min > 100, preço total: min - 100 * 2.
         int valorPlano;
         int minExtra;
         int minTotal;
         int valorExtra;
+        int valorTotal;
         valorPlano = 50;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o número de minutos consumidos: ");
+        minTotal = sc.nextInt();
+        if (minTotal < 0){
+            System.out.println("Número de minutos inválido.");
+            return;
+        }
         if (minTotal > 100){
             minExtra = minTotal - 100;
             valorExtra = minExtra*2;
-            
+            valorTotal = valorPlano + valorExtra;
+            System.out.println("O valor total da conta é: " + valorTotal);
+            System.out.println(valorExtra + " reais foram cobrados a mais por conta dos minutos extras.");
+            System.out.println(minExtra + " minutos foram cobrados a mais por conta dos minutos extras.");
+        } else {
+            valorTotal = valorPlano;
+            System.out.println("O valor total da conta é: " + valorTotal);
         }
+        sc.close();
     }
 }
